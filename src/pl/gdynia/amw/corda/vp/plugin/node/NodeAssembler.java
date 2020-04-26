1a3f7c87b216b81a836d6e92d490f10570647ab7
Jakub Bednarski <jakub.bednarski@gmail.com>
2020-04-26 21:53:37 +0200

    Read tagged values directly from Corda

--- src/pl/gdynia/amw/corda/vp/plugin/node/NodeAssembler.java
+++ src/pl/gdynia/amw/corda/vp/plugin/node/NodeAssembler.java
@@ -1,11 +1,21 @@
 package pl.gdynia.amw.corda.vp.plugin.node;
 
+import com.vp.plugin.ApplicationManager;
+import com.vp.plugin.ProjectManager;
+import com.vp.plugin.model.IModelElement;
 import com.vp.plugin.model.INode;
+import com.vp.plugin.model.IStereotype;
 import com.vp.plugin.model.ITaggedValue;
-import pl.gdynia.amw.dictionary.TaggedValuesDict;
+import com.vp.plugin.model.factory.IModelElementFactory;
 import pl.gdynia.amw.model.node.CordaNode;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class NodeAssembler {
+
+    private static final String UML_PROFILE_NAME = "UML Profile for Distributed Ledger Deployment";
 
     private static NodeAssembler INSTANCE = new NodeAssembler();
 
@@ -14,8 +24,17 @@ public class NodeAssembler {
     }
 
     public CordaNode buildNode(INode vpNode, CordaNode cordaNode) {
-        TaggedValuesDict.getInstance().getValues()
-                .forEach(tv -> cordaNode.getProperties().put(tv.getValue(), readTaggedValue(tv.getValue(), vpNode)));
+        ProjectManager projectManager = ApplicationManager.instance().getProjectManager();
+        Set<String> taggedValues = new HashSet<>();
+
+        IModelElement[] stereotypes = Stream.of(projectManager.getProject().getLinkedProjects()).filter(lp -> lp.getName().equals(UML_PROFILE_NAME)).findFirst().get().toModelElementArray(IModelElementFactory.MODEL_TYPE_STEREOTYPE);
+        Stream.of(stereotypes)
+                .map(modelElement -> (IStereotype) modelElement)
+                .map(stereotype -> stereotype.getTaggedValueDefinitions())
+                .map(taggedValueDefinitionContainer -> taggedValueDefinitionContainer.toTaggedValueDefinitionArray())
+                .forEach(taggedValuesDefinitions -> Stream.of(taggedValuesDefinitions).forEach(taggedValue -> taggedValues.add(taggedValue.getName())));
+
+        taggedValues.forEach(tv -> cordaNode.getProperties().put(tv, readTaggedValue(tv, vpNode)));
 
         cordaNode.setName(vpNode.getName());
 
